50f2249a324e324186d621612a92ab564e60704a
Manolo Carrasco <manolo@vaadin.com>
2024-07-03 14:14:23 +0200

    change vaadin button to rounded button

--- src/main/java/com/example/application/views/hello/HelloView.java
+++ src/main/java/com/example/application/views/hello/HelloView.java
@@ -3,9 +3,12 @@ package com.example.application.views.hello;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.dom.Style.Position;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -18,7 +21,12 @@ public class HelloView extends HorizontalLayout {
 
     public HelloView() {
         name = new TextField("Your name");
-        sayHello = new Button("Say hello");
+        sayHello = new Button();
+        sayHello.getElement().getStyle().setBorderRadius("100%");
+        sayHello.addClassName("fab");
+        sayHello.setIcon(VaadinIcon.LOCATION_ARROW.create());
+        sayHello.addThemeVariants(ButtonVariant.LUMO_ERROR, ButtonVariant.LUMO_PRIMARY);
+        
         sayHello.addClickListener(e -> {
             Notification.show("Hello " + name.getValue());
         });
