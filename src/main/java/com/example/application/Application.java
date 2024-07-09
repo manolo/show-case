bdd8d3118756872276de4d33217a8f32911758e9
Manolo Carrasco <manolo@vaadin.com>
2024-07-09 11:34:12 +0200

    use Locale from request instead of server setting

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,15 +1,21 @@
 package com.example.application;
 
-import com.example.application.data.SamplePersonRepository;
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.component.page.Push;
-import com.vaadin.flow.theme.Theme;
 import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
 import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import com.example.application.data.SamplePersonRepository;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.vaadin.flow.theme.Theme;
 
 /**
  * The entry point of the Spring Boot application.
@@ -19,12 +25,23 @@ import org.springframework.context.annotation.Bean;
  *
  */
 @SpringBootApplication
-@Theme(value = "show-case")
+@Theme("show-case")
 @Push
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Component
+    public static class ServiceInitListener implements VaadinServiceInitListener {
+        @Override
+        public void serviceInit(ServiceInitEvent e) {
+            // Sets the locale based on browser request instead of server system setting
+            e.getSource().addUIInitListener(uiInitEvent -> {
+                uiInitEvent.getUI().setLocale(VaadinService.getCurrentRequest().getLocale());
+            });
+        }
     }
 
     @Bean
